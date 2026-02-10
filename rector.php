9e19d11efb981ccff35d0ed4d607921c3e5b86fd
Peter Hatton <pete@monolight.org>
2026-02-10 18:03:50 +0000

    Adjust rector rules

--- rector.php
+++ rector.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 
 return RectorConfig::configure()
     ->withPaths([__DIR__ . '/src'])
-    ->withPhpSets(php84: true);
+    ->withPhpSets(php84: true)
+    ->withSkip([ClassPropertyAssignToConstructorPromotionRector::class]);
